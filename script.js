d3891c5097e719564d74823a95bc13a1ea4ef4c3
Papa1999 <thioubepapamacoumba@gmail.com>
2024-02-09 17:18:45 +0100

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Creéation de la fonctionnalité toogle 
 const toogleBtn= document.querySelector(".toogle-btn");
-const toogleMenu= document.querySelector(".toogle-menu")
+const toogleMenu= document.querySelector(".toogle-menu");
+const links= document.querySelectorAll(".toogle-menu a")
 
 toogleBtn.addEventListener("click", () => {
     toogleMenu.classList.toggle("open"); 
 })
+
+for(let i=0; i<=links.length; i++){
+    const link= links[i]; 
+    link.addEventListener("click", () => {
+        toogleMenu.classList.remove("open");
+    })
+}
